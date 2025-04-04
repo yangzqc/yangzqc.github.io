31dd5a48f80e2121d1e4429bf16b0a922f7ef70e
yangzqc <yangzqc@126.com>
2025-04-05 00:03:07 +0800

    feat: update .gitignore

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,20 +7,13 @@ import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
   lang: "en-US",
-
   title: "yangzqc",
-
   description: "Yang Ziqiang's Wonderful Journey",
-
   base: "/",
-
   theme: defaultTheme({
     logo: "https://vuejs.press/images/hero.png",
-
     navbar: ["/", "/beijing/index", "/shenzhen/index"],
   }),
-
   plugins: [photoSwipePlugin(), nprogressPlugin(), mediumZoomPlugin()],
-
   bundler: viteBundler(),
 });
